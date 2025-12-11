39be83d50ea577616593b7dca0d61db6ff7ad6f8
SAATAAP <gabriel.blancosoria4001@gmail.com>
2025-12-11 10:52:59 +0100

    Tiny conf db

--- backend/bd.php
+++ backend/bd.php
@@ -1,5 +1,14 @@
 <?php 
 
-    
+    function connectDatabase (){
+
+        $dbname = "";
+        $dbhost = "";
+        $dbuser = "";
+        $dbpass = "";
+        
+        $pdo = new PDO("mysql:");
+
+    }
 
 ?>
