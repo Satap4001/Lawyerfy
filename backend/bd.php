7ca072687ad43881ad7659c67a9a1c9520c6301a
SAATAAP <gabriel.blancosoria4001@gmail.com>
2025-12-15 10:40:27 +0100

    Cambios funciones_bd

--- backend/bd.php
+++ backend/bd.php
@@ -2,13 +2,34 @@
 
     function connectDatabase (){
 
-        $dbname = "";
-        $dbhost = "";
-        $dbuser = "";
+        $dbname = "lawyerfy_bd";
+        $dbhost = "localhost";
+        $dbuser = "root";
         $dbpass = "";
         
-        $pdo = new PDO("mysql:");
+        $pdo = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8",$dbuser,$dbpass);
 
+        return $pdo;
+    }
+
+    function getAllEspecialidades (){
+        $pdo = connectDatabase();
+
+        $stmt = $pdo->prepare("SELECT * FROM especialidad");
+
+        $resultado =  $stmt->execute();
+
+        return $resultado;
+    }
+
+    function getEspecialidadById($id){
+        $pdo = connectDatabase();
+
+        $stmt= $pdo->prepare("SELECT * FROM especialidad where id like :id");
+
+        $resultado = $stmt->execute(["id" => $id]);
+
+        return $resultado;
     }
 
 ?>
