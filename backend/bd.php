223af89aae5e4cfc379cb4d65028e086b6d21ce5
SAATAAP <gabriel.blancosoria4001@gmail.com>
2025-12-18 10:55:17 +0100

    Mas funciones bases de datos

--- backend/bd.php
+++ backend/bd.php
@@ -14,21 +14,29 @@
 
     function getAllEspecialidades (){
         $pdo = connectDatabase();
-
         $stmt = $pdo->prepare("SELECT * FROM especialidad");
-
         $resultado =  $stmt->execute();
-
         return $resultado;
     }
 
     function getEspecialidadById($id){
         $pdo = connectDatabase();
-
         $stmt= $pdo->prepare("SELECT * FROM especialidad where id like :id");
-
         $resultado = $stmt->execute(["id" => $id]);
+        return $resultado;
+    }
 
+    function getHistoricoByUserId($id){
+        $pdo = connectDatabase();
+        $stmt = $pdo->prepare("SELECT * FROM historico where cliente_id like :id");
+        $resultado = $stmt->execute(["id" => $id]);
+        return $resultado;
+    }
+
+    function getAbogadoByEspecialidadId($id){
+        $pdo = connectDatabase();
+        $stmt = $pdo->prepare("SELECT * FROM abogado where id_especialidad like :id");
+        $resultado = $stmt->execute(["id" => $id]);
         return $resultado;
     }
 
