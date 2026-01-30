dbb2f520d53f78bc3d9317cd0c7b5e4f66f332e3
Dangt293 <danigt935@gmail.com>
2026-01-30 09:23:21 +0100

    El perfil de abogado ahora muestra sus datos

--- backend/bd.php
+++ backend/bd.php
@@ -158,14 +158,13 @@
 
         try {
             // Preparar la consulta
-            $sql = "INSERT INTO publicacion (fecha, titulo, descripcion, codigoImagen, id_admin, id_abogado) 
-                    VALUES (:fecha, :titulo, :descripcion, :codigoImagen, :id_admin, :id_abogado)";
+            $sql = "INSERT INTO publicacion (titulo, descripcion, codigoImagen, id_admin, id_abogado) 
+                    VALUES (:titulo, :descripcion, :codigoImagen, :id_admin, :id_abogado)";
 
             $stmt = $pdo->prepare($sql);
 
             // Ejecutar con parámetros
             $stmt->execute([
-                ':fecha' => time(),
                 ':titulo' => $titulo,
                 ':descripcion' => $contenido,
                 ':codigoImagen' => $imagenNombre,
@@ -213,4 +212,22 @@
         return $resultado;
     }
 
+
+    function getDatosAbo($id) {
+        $pdo = connectDatabase();
+        $stmt = $pdo->prepare("SELECT 
+                        a.nombre as nombre,
+                        a.apellido as apellido,
+                        a.nacionalidad as nacionalidad,
+                        a.telefono as telefono,
+                        a.genero as genero,
+                        a.localidad as localidad,
+                        e.nombre AS especialidad
+                    FROM abogado a
+                    JOIN especialidad e ON a.id_especialidad = e.id
+                    WHERE a.id = :id");
+        $stmt->execute(["id" => $id]);
+        $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $resultado;
+    }
 ?>
